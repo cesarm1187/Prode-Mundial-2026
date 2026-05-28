39f511dfc66fc1800c9237a42ded71280954c063
Ultramarine de Racing <cesarm1187@gmail.com>
2026-05-28 13:15:22 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='prode-mundial-v9-fixture';
+const CACHE_NAME='prode-mundial-v10-fixture';
 const APP_ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_ASSETS)));
